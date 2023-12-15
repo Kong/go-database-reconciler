9a344ae075f8754215679f160e3c16385203a7f3
Travis Raines <571832+rainest@users.noreply.github.com>
2023-12-15 13:51:32 -0800

    fix: wait for Kong to come up better

--- .ci/setup_kong.sh
+++ .ci/setup_kong.sh
@@ -63,3 +63,4 @@ docker run -d --name $GATEWAY_CONTAINER_NAME \
   $KONG_IMAGE
 
 waitContainer "Kong" $GATEWAY_CONTAINER_NAME "kong health"
+curl --retry 12 --retry-all-errors http://localhost:8001
--- .ci/setup_kong_ee.sh
+++ .ci/setup_kong_ee.sh
@@ -127,3 +127,4 @@ docker run -d --name $GATEWAY_CONTAINER_NAME \
   $KONG_IMAGE
 
 waitContainer "Kong" $GATEWAY_CONTAINER_NAME "kong health"
+curl --retry 12 --retry-all-errors http://localhost:8001
