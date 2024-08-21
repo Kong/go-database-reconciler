399d2dbb997627e4eb479b328afe43fb5b1fb480
Antoine Jacquemin <ajacquem@gmail.com>
2024-08-21 17:36:11 +0200

    init

--- pkg/file/builder.go
+++ pkg/file/builder.go
@@ -187,11 +187,11 @@ func (b *stateBuilder) consumerGroups() {
 			}
 		}
 		sort.Strings(stringTags)
-		sort.Strings(b.lookupTagsConsumers)
+		sort.Strings(b.lookupTagsConsumerGroups)
 		// if the consumer tags and the lookup tags are the same, it means
 		// that the consumer is a global consumer retrieved from upstream,
 		// therefore we don't want to merge its tags with the selected tags.
-		if !reflect.DeepEqual(stringTags, b.lookupTagsConsumers) {
+		if !reflect.DeepEqual(stringTags, b.lookupTagsConsumerGroups) {
 			utils.MustMergeTags(&cg.ConsumerGroup, b.selectTags)
 		}
 
