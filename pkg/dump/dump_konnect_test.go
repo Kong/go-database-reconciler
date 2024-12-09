718b131ad3407335b068dc053729c586cd5c8109
Prashansa Kulshrestha <prashkulshrestha@gmail.com>
2024-12-09 15:30:36 +0530

    chore: fixed linting issues

--- pkg/dump/dump_konnect_test.go
+++ pkg/dump/dump_konnect_test.go
@@ -437,7 +437,6 @@ func Test_excludeKonnectManagedEntities(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := excludeKonnectManagedEntities(tt.entities)
-
 			if err != nil {
 				t.Errorf("excludeKonnectManagedEntities() error = %v", err)
 			}
