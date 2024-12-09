9cb130c33f59b53d6c7b754911d20aece58b98b5
Prashansa Kulshrestha <prashkulshrestha@gmail.com>
2024-12-09 10:36:18 +0530

    fix: ensuring consumer-group scoped plugins show tags
    On running `deck file render` command, tags
    were coming up for global and service/route
    scoped plugins, but not for consumer-group
    scoped ones. This was because the underlying
    struct that were using for writer did not have
    `Tags` as a field. This change was added in the
    underlying type in go-kong.
    
    This change upgrades the go-kong version to
    v0.61.0 and adds `Tags` field in all missing
    places while building consumer-group scoped
    plugins or writing them to file.
    For: https://konghq.atlassian.net/browse/FTI-6292

--- pkg/state/builder.go
+++ pkg/state/builder.go
@@ -125,6 +125,7 @@ func buildKong(kongState *KongState, raw *utils.KongRawState) error {
 						Config:        p.Config,
 						ConsumerGroup: cg.ConsumerGroup,
 						ConfigSource:  p.ConfigSource,
+						Tags:          p.Tags,
 					},
 				},
 			)
