31425d4faa918a0e985ed86f1d544bb4be16c466
Harsha Dixit <harsha.dixit@konghq.com>
2025-03-28 12:39:06 +0530

    chore: bump go-kong to latest version

--- pkg/types/utils.go
+++ pkg/types/utils.go
@@ -4,6 +4,6 @@ import (
 	"fmt"
 )
 
-func errDuplicateEntity(entityType string, entityId string) error {
-	return fmt.Errorf("error: %s with ID %s already exists", entityType, entityId)
+func errDuplicateEntity(entityType string, entityID string) error {
+	return fmt.Errorf("error: %s with ID %s already exists", entityType, entityID)
 }
