07ea2e035902e138ddc656eb16ef57b248e69c44
Manish S V Kumar <manish.kumar@konghq.com>
2026-06-15 21:37:23 +0530

    optimizing the test cases

--- pkg/diff/diff_test.go
+++ pkg/diff/diff_test.go
@@ -5,33 +5,20 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func newTestSyncer(t *testing.T) *Syncer {
-	sc, err := NewSyncer(SyncerOpts{})
-	require.NoError(t, err)
-	return sc
-}
 
 func TestSolve_InvalidParallelism_Zero(t *testing.T) {
 	parallelism := 0
-	t.Run("", func(t *testing.T) {
-		sc := newTestSyncer(t)
-		go func() {
-			_, errs, _ := sc.Solve(context.Background(), parallelism, true, false)
-			assert.Equal(t, 1, len(errs), "Solve should return exactly one error for parallelism=%d", parallelism)
-		}()
-	})
+	sc, _ := NewSyncer(SyncerOpts{})
+	_, errs, _ := sc.Solve(context.Background(), parallelism, true, false)
+	assert.EqualError(t, errs[0], "parallelism can not be less than 1")
+	assert.Equal(t, 1, len(errs), "Solve should return exactly one error for parallelism=%d", parallelism)
 }
 
 func TestSolve_InvalidParallelism_Negative(t *testing.T) {
 	parallelism := -1
-	t.Run("", func(t *testing.T) {
-		sc := newTestSyncer(t)
-		go func() {
-			_, errs, _ := sc.Solve(context.Background(), parallelism, true, false)
-			assert.Equal(t, 1, len(errs), "Solve should return exactly one error for parallelism=%d", parallelism)
-		}()
-	})
+	sc, _ := NewSyncer(SyncerOpts{})
+	_, errs, _ := sc.Solve(context.Background(), parallelism, true, false)
+	assert.EqualError(t, errs[0], "parallelism can not be less than 1")
+	assert.Equal(t, 1, len(errs), "Solve should return exactly one error for parallelism=%d", parallelism)
 }
